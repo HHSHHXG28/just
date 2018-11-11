a1d83158ae6f0fac0aa79ca18b01ffd5fe9a0365
HHSHHXG28 <44509375+HHSHHXG28@users.noreply.github.com>
2018-11-11 15:31:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,6 +50,27 @@ msg.delete();
 }
 });
 
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+        let room = m.guild.channels.find("name","just");
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+    
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+	setTimeout(() => {
+  m.ban() .then(() => {
+    room.send(`**:no_entry: | ${m} Has been banned for: \`fake\`**`)
+  });
+}, 500);
+});
+
 client.on("message", (message) => {
     var wait = new Set();
   if (message.content.startsWith(prefix + "new")) {     
